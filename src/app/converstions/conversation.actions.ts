b01bccb23f1a50567c692d4d7b1839f08ca02c7a
abraxis90 <ia.virtaci@gmail.com>
2019-08-10 13:16:52 +0200

    feat(app): add basic actions

---
+++ src/app/converstions/conversation.actions.ts
@@ -0,0 +1,62 @@
+import { Action } from '@ngrx/store';
+import { Conversation } from './coversation';
+import { Message } from './message';
+
+export enum ConversationActionTypes {
+  ConversationLoad = 'conversation.load',
+  ConversationLoadSuccess = 'conversation.load.success',
+  ConversationAdd = 'conversation.add',
+  ConversationAddSuccess = 'conversation.add.success',
+  ConversationMessageSend = 'conversation.message.send',
+  ConversationMessageSendSuccess = 'conversation.message.send.success'
+}
+
+export type ConversationActions =
+  ConversationLoad
+  | ConversationLoadSuccess
+  | ConversationAdd
+  | ConversationAddSuccess
+  | ConversationMessageSend
+  | ConversationMessageSendSuccesss;
+
+/* LOAD */
+export class ConversationLoad implements Action {
+  readonly type = ConversationActionTypes.ConversationLoad;
+}
+
+export class ConversationLoadSuccess implements Action {
+  readonly type = ConversationActionTypes.ConversationLoadSuccess;
+
+  constructor(public payload: Conversation[]) {
+  }
+}
+
+/* ADD */
+export class ConversationAdd implements Action {
+  readonly type = ConversationActionTypes.ConversationAdd;
+
+  constructor(public payload: Conversation) {
+  }
+}
+
+export class ConversationAddSuccess implements Action {
+  readonly type = ConversationActionTypes.ConversationAddSuccess;
+
+  constructor(public payload: Conversation) {
+  }
+}
+
+/* MESSAGE SEND */
+export class ConversationMessageSend implements Action {
+  readonly type = ConversationActionTypes.ConversationMessageSend;
+
+  constructor(public payload: Message) {
+  }
+}
+
+export class ConversationMessageSendSuccesss implements Action {
+  readonly type = ConversationActionTypes.ConversationMessageSendSuccess;
+
+  constructor(public payload: Message) {
+  }
+}
