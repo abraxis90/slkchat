e3c1cc91e00a484f2c154a8b2f2449169bb1ddee
abraxis90 <ia.virtaci@gmail.com>
2019-10-27 01:30:22 +0200

    feat(app): add back button

--- src/app/reusables/components/toolbar/toolbar.component.ts
+++ src/app/reusables/components/toolbar/toolbar.component.ts
@@ -1,7 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { User } from 'firebase';
+import { NavigationEnd, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 import { AuthenticationService } from '../../../services/auth/authentication.service';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-toolbar',
@@ -11,8 +14,18 @@ import { AuthenticationService } from '../../../services/auth/authentication.ser
 export class ToolbarComponent {
 
   @Input() currentUser: User;
+  public canNavigateBack$: Observable<boolean> = this.router.events
+    .pipe(
+      filter(routerEvent => routerEvent instanceof NavigationEnd),
+      map((routerEvent: any) => {
+        const url = routerEvent.urlAfterRedirects ? routerEvent.urlAfterRedirects : routerEvent.url;
+        return url !== '/conversations';
+      })
+    );
 
-  constructor(private auth: AuthenticationService) {}
+  constructor(private auth: AuthenticationService,
+              private router: Router) {}
+
 
   logout(): void {
     this.auth.logout();
